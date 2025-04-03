9bec759d8d5085bfb26fc8e188362f84f8f42e29
SongKyuSeob <sks5075@naver.com>
2025-04-04 00:42:09 +0900

    chore: 프로젝트 init

---
+++ CodeReviewTest/CodeReviewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeReviewTest
+//
+//  Created by 송규섭 on 4/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
